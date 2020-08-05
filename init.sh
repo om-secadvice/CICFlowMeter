f0f487ccc670a988a838add7f003d40ccdb0932a
Hariom <om.secadvice@gmail.com>
2020-08-05 18:50:50 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,12 +10,10 @@ echo 'export PATH=$PATH:$JAVA_HOME/bin' >> ~/.bash_aliases
 # Install libpcap-dev
 sudo apt-get -y install libpcap
 
-# Clone CICFlowmeter from github
-git clone https://github.com/om-secadvice/CICFlowMeter.git
 cd CICFlowMeter/jnetpcap/linux/jnetpcap-1.4.r1425/
 sudo cp libjnetpcap.so libjnetpcap-100.so /usr/lib/
 mvn install:install-file -Dfile=jnetpcap.jar -DgroupId=org.jnetpcap -DartifactId=jnetpcap -Dversion=1.4.1 -Dpackaging=jar
 cd ../../../
-./gradlew exeCMD
+gradle exeCMD
 
 
